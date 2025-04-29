2c450a457dd08f47d7ae0292a6ae1ef325ac06d8
RaillyLinker <raillylinker@gmail.com>
2025-04-29 20:14:36 +0900

    feat : init project

---
+++ src/main/java/com/raillylinker/Java_SpringbootMvc_Template/JavaSpringbootMvcTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.raillylinker.Java_SpringbootMvc_Template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootMvcTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootMvcTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raillylinker/Java_SpringbootMvc_Template/JavaSpringbootMvcTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raillylinker.Java_SpringbootMvc_Template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootMvcTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
